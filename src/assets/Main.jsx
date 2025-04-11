6a972c33ed3475aa88f3341f35d54bca664f6476
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-11 16:17:27 +0200

    feat: complete first milestone

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 
 // Main 
 
 // indirizzo API delle attrici 
-const endpoint = 'https://freetestapi.com/api/v1/actresses';
+const endpoint = 'https://www.freetestapi.com/api/v1/actresses';
 
 export default function Main() {
 
+    const [actresses, setActresses] = useState([]);
+
+    function fetchActresses() {
+
+        // fare chiamata axios
+        axios.get(endpoint)
+            .then(res => console.log(res.data)) // => stampare in console la lista delle attrici
+    }
+
+    useEffect(fetchActresses, []);
+
     return (
         <main>
-
 
         </main>
     )
