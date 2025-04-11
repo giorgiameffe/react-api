1b61af03c9a6391d4815782065dde5ca1c5d3577
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-11 15:59:24 +0200

    feat: install axios and save endpoint

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -1,9 +1,16 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 // Main 
+
+// indirizzo API delle attrici 
+const endpoint = 'https://freetestapi.com/api/v1/actresses';
 
 export default function Main() {
 
     return (
         <main>
+
 
         </main>
     )
