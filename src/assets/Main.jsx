1ae2a0a333409a4840baecb97439da3d553ebba8
giorgiameffe <giorgiameffe03@gmail.com>
2025-04-11 23:14:01 +0200

    feat: complete second bonus

--- src/assets/Main.jsx
+++ src/assets/Main.jsx
@@ -13,56 +13,42 @@ const actorsEndpoint = 'https://www.freetestapi.com/api/v1/actors';
 
 export default function Main() {
 
-    const [actresses, setActresses] = useState([]);
-    const [actors, setActors] = useState([]);
+    const [singleList, setSingleList] = useState([]);
 
-    function fetchActresses() {
+    function fetchSingleList() {
 
         // chiamata API per ricevere dati sulle attrici
         axios.get(actressesEndpoint)
-            .then(res => setActresses(res.data));
+            .then(actressesResponse => {
+
+                // chiamata API per ricevere dati sugli attori
+                axios.get(actorsEndpoint)
+                    .then(actorsResponse => {
+
+                        const actressesList = actressesResponse.data;
+                        const actorsList = actorsResponse.data;
+
+                        setSingleList([...actressesList, ...actorsList]);
+                    })
+            })
     }
 
-    useEffect(fetchActresses, []);
-
-    function fetchActors() {
-
-        // chiamata API per ricevere dati sugli attori 
-        axios.get(actorsEndpoint)
-            .then(res => setActors(res.data));
-    }
-
-    useEffect(fetchActors, []);
+    useEffect(fetchSingleList, []);
 
     return (
         <main>
             <div className="container">
-                <h2>Actresses</h2>
+                <h2>Actresses and Actors</h2>
                 <div className='cards-raw'>
-                    {actresses.map(actress =>
-                        <Card key={actress.id} name={actress.name} onChange={event => setActresses(event.target.value)}>
+                    {singleList.map(actors =>
+                        <Card key={actors.name} name={actors.name} onChange={event => setSingleList(event.target.value)}>
                             <figure className='card-img'>
-                                <img src={actress.image} alt={actress.name} />
+                                <img src={actors.image} alt={actors.name} />
                             </figure>
-                            <li> <span>Birth Year:</span> {actress.birth_year}</li>
-                            <li> <span>Nationality:</span>  {actress.nationality}</li>
-                            <li> <span>Biography:</span> {actress.biography}</li>
-                            <li> <span>Awards:</span> {actress.awards}</li>
-                        </Card>
-                    )}
-                </div>
-
-                <h2>Actors</h2>
-                <div className='cards-raw'>
-                    {actors.map(actor =>
-                        <Card key={actor.id} name={actor.name} onChange={event => setActors(event.target.value)}>
-                            <figure className='card-img'>
-                                <img src={actor.image} alt={actor.name} />
-                            </figure>
-                            <li> <span>Birth Year:</span> {actor.birth_year}</li>
-                            <li> <span>Nationality:</span>  {actor.nationality}</li>
-                            <li> <span>Biography:</span> {actor.biography}</li>
-                            <li> <span>Awards:</span> {actor.awards}</li>
+                            <li> <span>Birth Year:</span> {actors.birth_year}</li>
+                            <li> <span>Nationality:</span>  {actors.nationality}</li>
+                            <li> <span>Biography:</span> {actors.biography}</li>
+                            <li> <span>Awards:</span> {actors.awards}</li>
                         </Card>
                     )}
                 </div>
